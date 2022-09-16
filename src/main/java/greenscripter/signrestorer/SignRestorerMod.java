fb8a029f75044e7c132b1ed60ee24c82986332df
Barrulik <barrulik@gmail.com>
2022-09-16 14:53:21 +0300

    A HUGE upgrade
    Here is the list of changes:
    1. Sign list moved to a file hosted on github.
    2. Added a keybind (B) to disable/enable the mod
    3. Updated minecraft version to 1.19.2 from 1.18.2.

--- src/main/java/greenscripter/signrestorer/SignRestorerMod.java
+++ src/main/java/greenscripter/signrestorer/SignRestorerMod.java
@@ -1,10 +1,27 @@
 package greenscripter.signrestorer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -12,25 +29,61 @@ import com.google.gson.reflect.TypeToken;
 import net.fabricmc.api.ModInitializer;
 
 public class SignRestorerMod implements ModInitializer {
-	
-	public static Map<String, SignData> signData = new HashMap<>();
-	
+	public static final Logger LOGGER = LoggerFactory.getLogger("sign-restorer");
+	public static List<Map<String, SignData>> signData = null;
+	public static boolean enabled = false;
+	private static KeyBinding stateChangeKeybind;
+
+
 	@Override
 	public void onInitialize() {
-		reloadSignData();
+		try {
+			reloadSignData();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+
+
+		stateChangeKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+						"Change Sign Restorer State", // The translation key of the keybinding's name
+						InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+						GLFW.GLFW_KEY_B, // The keycode of the key
+						"Sign Restorer" // The translation key of the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (stateChangeKeybind.wasPressed()) {
+				enabled =!enabled;
+				client.player.sendMessage(Text.literal("§7 Sign Restorer is now " + (enabled ? "enabled":"disabled")), false);
+			}
+		});
 	}
 	
-	public static void reloadSignData() {
-		File f = new File("signdata.json");
-		String fileData;
+	public static void reloadSignData() throws IOException {
+		String fileData = null;
 		try {
-			fileData = Files.lines(f.toPath()).reduce((s1, s2) -> s1 + " " + s2).orElse(null);
 			Gson gson = new Gson();
-			signData = gson.fromJson(fileData, new TypeToken<Map<String, SignData>>() {
-			}.getType());
+			fileData = fetchFile();
+			signData = gson.fromJson(fileData, new TypeToken<ArrayList<Map<String, SignData>>>(){}.getType());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	public static String fetchFile() throws IOException {
+		URL url = new URL("https://raw.githubusercontent.com/barrulik/sign-restorer-data/main/signData.json");
+		URLConnection urlConnection = url.openConnection();
+		urlConnection.setConnectTimeout(1000);
+		urlConnection.setReadTimeout(1000);
+		BufferedReader breader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+		String output = "";
+		String line;
+		while((line = breader.readLine()) != null) {
+			output+=line;
+		}
+
+		return output;
 	}
 }
--- src/main/java/greenscripter/signrestorer/mixin/SignEditScreenMixin.java
+++ src/main/java/greenscripter/signrestorer/mixin/SignEditScreenMixin.java
@@ -1,5 +1,6 @@
 package greenscripter.signrestorer.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,6 +13,8 @@ import greenscripter.signrestorer.SignRestorerMod;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.List;
 
 @Mixin(SignEditScreen.class)
 public abstract class SignEditScreenMixin {
@@ -26,10 +29,20 @@ public abstract class SignEditScreenMixin {
 	
 	@Inject(at = { @At("HEAD") }, method = { "init()V" })
 	private void onInit(CallbackInfo ci) {
+		if (!SignRestorerMod.enabled) return;
+
 		BlockPos pos = sign.getPos();
-		SignData data = SignRestorerMod.signData.get(pos.getX() + " " + pos.getY() + " " + pos.getZ());
-		if (data == null) return;
-		
+		SignData data = null;
+		SignData tempData = null;
+		for (int i=0;i<SignRestorerMod.signData.size();i++){
+			tempData = SignRestorerMod.signData.get(i).get(pos.getX() + " " + pos.getY() + " " + pos.getZ());
+			if (tempData != null){
+				data = tempData;
+			}
+		}
+		if (data==null) return;
+
+
 		for (int i = 0; i < 4; i++)
 			text[i] = data.text[i];
 		
